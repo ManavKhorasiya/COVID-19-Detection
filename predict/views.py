398ee74cdc5edd5faf0e8922de47af6cd2ef8863
ManavKhorasiya <manavkhorasiya@gmail.com>
2020-07-30 00:38:38 +0530

    base64

--- predict/views.py
+++ predict/views.py
@@ -159,7 +159,7 @@ def android_predict(request):
     if request.method == 'POST' :
         print('method is POST')
         print('Body is : ' + str(request.body))
-        decoded = request.body.decode("UTF-8")
+        decoded = request.body.decode("base64")
         print(decoded)
         name_image = decoded.split('&')[1].split('=')[1]
         print('name is : ' + name_image)
